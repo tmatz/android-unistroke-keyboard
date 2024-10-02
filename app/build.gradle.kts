82aec6ab565a30fa78337f0f9478f8d3fdfe2ed8
MATSUDA Takashi <tmatz.dev@gmail.com>
2024-10-03 01:19:50 +0900

    migrate to AndroidIDE

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "io.github.tmatz.hackers_unistroke_keyboard"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "io.github.tmatz.hackers_unistroke_keyboard"
+        minSdk = 21
+        targetSdk = 26
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = File(rootProject.projectDir, "release.keystore")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+            storePassword = System.getenv("STORE_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // implementation("com.google.android.material:material:1.9.0")
+    // implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // implementation("androidx.appcompat:appcompat:1.6.1")
+}
