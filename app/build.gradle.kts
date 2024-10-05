fece69440e2f932381be8f845ad7292319ef7017
MATSUDA Takashi <tmatz.dev@gmail.com>
2024-10-05 15:34:22 +0900

    reopen closed input method from notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,5 @@ android {
 dependencies {
     // implementation("com.google.android.material:material:1.9.0")
     // implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    // implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
